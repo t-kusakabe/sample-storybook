e14dc32f6632f99fea185d52fde70efca519aad3
t-kusakabe <ttmac0704@gmail.com>
2022-04-18 18:07:54 +0900

    storybookのデザインをtailwindで引き継ぐ

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,32 +1,13 @@
 import React from 'react';
-import './button.css';
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
+export interface ButtonProps {
   primary?: boolean;
-  /**
-   * What background color to use
-   */
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
   primary = false,
   size = 'medium',
@@ -34,11 +15,28 @@ export const Button = ({
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
+  const baseButton = 'rounded-full font-bold';
+  const sizeMode = size === 'small'
+    ? 'py-1.5 px-4 text-xs'
+    : size === 'medium'
+    ? 'py-2 px-5 text-sm'
+    : size === 'large'
+    ? 'py-3 px-6 text-base'
+    : '';
+
+  return primary ? (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={`text-white bg-blue-450 ${baseButton} ${sizeMode}`}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={`text-gray-600 bg-transparent shadow-inner ${baseButton} ${sizeMode}`}
       style={{ backgroundColor }}
       {...props}
     >
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          450: '#1ea7fd'
+        }
+      }
+    },
+    fontFamily: {
+      sans: ['Nunito Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif']
+    },
+    boxShadow: {
+      inner: 'rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset'
+    }
+  },
+  variants: {
+    extend: {}
   },
   plugins: [],
 }
